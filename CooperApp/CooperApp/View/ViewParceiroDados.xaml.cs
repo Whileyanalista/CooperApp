fe1c5427cae3823f2a71ee24f090a8e5ed8ecb66
Whileyanalista <whiley.analista@gmail.com>
2024-03-11 10:08:58 -0300

    conclusão de ajuste dados App

--- CooperApp/CooperApp/View/ViewParceiroDados.xaml.cs
+++ CooperApp/CooperApp/View/ViewParceiroDados.xaml.cs
@@ -6,20 +6,33 @@ namespace CooperApp.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ViewParceiro : ContentView
     {
+        public static BindableProperty DadosParceirosProperty = BindableProperty.Create("DadosParceiros", typeof(string), typeof(ViewParceiro));
         public static BindableProperty ComponenteParceiroProperty = BindableProperty.Create("ComponenteParceiro", typeof(string), typeof(ViewParceiro));
         public static BindableProperty TipoDocProperty = BindableProperty.Create("TipoDoc", typeof(string), typeof(ViewParceiro));
+        public static BindableProperty TipoDadoProperty = BindableProperty.Create("TipoDado", typeof(string), typeof(ViewParceiro));
 
+        public string DadosParceiros
+        {
+            get { return (string)GetValue(DadosParceirosProperty); }
+            set { SetValue(DadosParceirosProperty, value); }
+        }
 
         public string ComponenteParceiro
         {
             get { return (string)GetValue(ComponenteParceiroProperty); }
             set { SetValue(ComponenteParceiroProperty, value); }
         }
-
         public string TipoDoc
         {
             get { return (string)GetValue(TipoDocProperty); }
             set { SetValue(TipoDocProperty, value); }
+        }
+
+
+        public string TipoDado
+        {
+            get { return (string)GetValue(TipoDadoProperty); }
+            set { SetValue(TipoDadoProperty, value); }
         }
 
         public ViewParceiro()
